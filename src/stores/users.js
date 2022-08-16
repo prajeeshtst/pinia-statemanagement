48c5c5df08ba526e12d03574a7b80818bfac2d52
Prajeesh P <prajeesh+3@techgentsia.com>
2022-08-16 16:33:07 +0530

    Pinia state management with vite and vue 3

---
+++ src/stores/users.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { v4 as uuid } from 'uuid'
+
+
+export const useUserStore = defineStore("users", {
+
+    state: () => {
+        return {
+            users: []
+        }
+    },
+    actions: {
+        create(user) {
+            this.users.push({
+                id: uuid(),
+                ...user
+            })
+        },
+        delete(id) {
+            this.users = this.users.filter(user => user.id != user)
+        }
+    },
+    getters: {
+        usersByName(state) {
+			const sortable = [...state.users]
+			return sortable.sort((a, b) => a.name.localeCompare(b.name))
+		}
+    }
+
+})
